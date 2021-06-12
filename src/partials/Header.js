60bd23a7c41ce6eab49e23c5bb12f6baebbe8f53
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-13 00:55:13 +0600

    again

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from './partials/Logo';
 import pdf from '../assets/docs/pricelist.pdf'
+import Logo from './logo/logo'
 
 function Header() {
 
@@ -24,9 +24,9 @@ function Header() {
           {/* Site branding */}
           <div className="flex-shrink-0 mr-4">
             {/* Logo */}
-            <Logo />
-            {/* <Link to="/" className="block" aria-label="Cruip">
-              <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+            <Link to="/" className="block" aria-label="Cruip">
+              <Logo />
+              {/* <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <defs>
                   <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="header-logo">
                     <stop stopColor="#4FD1C5" offset="0%" />
@@ -35,8 +35,8 @@ function Header() {
                   </radialGradient>
                 </defs>
                 <rect width="32" height="32" rx="16" fill="url(#header-logo)" fillRule="nonzero" />
-              </svg>
-            </Link> */}
+              </svg> */}
+            </Link>
           </div>
 
           {/* Site navigation */}

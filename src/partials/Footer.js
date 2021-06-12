dd1909058b2cb22bbef3718c4e9e0cce3fdcaa5d
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-13 00:43:05 +0600

    new

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -152,7 +152,7 @@ function Footer() {
           </ul>
 
           {/* Copyrights note */}
-          <div className="text-sm text-gray-600 mr-4">Все права защищены. ©<a className="text-blue-600 hover:underline" href="https://skmedlab.kz">SKMEDLAB.KZ</a>2021</div>
+          <div className="text-sm text-gray-600 mr-4">Все права защищены. ©<a className="text-blue-600 hover:underline" href="https://skmedlab.kz">SKMEDLAB.KZ</a> 2021</div>
 
         </div>
 
--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './partials/Logo';
 import pdf from '../assets/docs/pricelist.pdf'
 
 function Header() {
@@ -23,7 +24,8 @@ function Header() {
           {/* Site branding */}
           <div className="flex-shrink-0 mr-4">
             {/* Logo */}
-            <Link to="/" className="block" aria-label="Cruip">
+            <Logo />
+            {/* <Link to="/" className="block" aria-label="Cruip">
               <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <defs>
                   <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="header-logo">
@@ -34,7 +36,7 @@ function Header() {
                 </defs>
                 <rect width="32" height="32" rx="16" fill="url(#header-logo)" fillRule="nonzero" />
               </svg>
-            </Link>
+            </Link> */}
           </div>
 
           {/* Site navigation */}

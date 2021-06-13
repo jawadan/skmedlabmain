a13a075e287264f672f24ece3611ecee7fef0945
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-14 04:18:26 +0600

    top h

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import TopHeader from '../partials/TopHeader';
 import Header from '../partials/Header';
 import HeroHome from '../partials/HeroHome';
 import FeaturesHome from '../partials/Features';
@@ -12,6 +12,7 @@ function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
 
+      <TopHeader />
       {/*  Site header */}
       <Header />
 

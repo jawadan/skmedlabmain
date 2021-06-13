7ec02d5a99d826fc215dd411b7c0826308b5e1e9
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-14 05:15:42 +0600

    Update Header.js

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -16,8 +16,7 @@ function Header() {
   }, [top]);
 
   return (
-    <header>
-      <div className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white blur shadow-lg'}`}>
+    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white blur shadow-lg'}`}>
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
 
@@ -63,7 +62,6 @@ function Header() {
           </nav>
 
         </div>
-      </div>
       </div>
     </header>
   );

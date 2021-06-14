4be01cac054111f80fea55663f9ba766a62d05eb
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-15 00:23:36 +0600

    back

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -67,26 +67,27 @@ function Features() {
 
           {/* Slider */}
           <div>
-          <Slider className="slider-wrapper">
-            {slides.map((item, index) => 
-            <div
-              key={index}
-              className="slider-content"
-              style={{ background: `url('${item.image}') no-repeat center center` }}
-            >
-              <div className="inner">
-                <h1>{item.title}</h1>
-                <p>{item.description}</p>
-                <button>{item.button}</button>
-              </div>
-              <section>
-                <img src={item.userProfile} alt={item.user} />
-                <span>
-                  Posted by <strong>{item.user}</strong>
-                </span>
-              </section>
-            </div>)}
-          </Slider>
+            <Slider className="slider-wrapper">
+              {content.map((item, index) => (
+                <div
+                  key={index}
+                  className="slider-content"
+                  style={{ background: `url('${item.image}') no-repeat center center` }}
+                >
+                  <div className="inner">
+                    <h1>{item.title}</h1>
+                    <p>{item.description}</p>
+                    <button>{item.button}</button>
+                  </div>
+                  <section>
+                    <img src={item.userProfile} alt={item.user} />
+                    <span>
+                      Posted by <strong>{item.user}</strong>
+                    </span>
+                  </section>
+                </div>
+              ))}
+            </Slider>
           </div>
 
           {/* Section content */}

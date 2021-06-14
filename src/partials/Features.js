b922d9f736eb61d1e67049c72180704e2f79e72d
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-15 02:55:18 +0600

    top

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -75,9 +75,9 @@ function Features() {
                   style={{ background: `url('${item.image}') no-repeat center center` }}
                 >
                   <div className="inner">
-                    <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">{item.title}</h1>
+                    <h1 className="text-3xl md:text-4xl font-extrabold leading-tighter tracking-tighter mb-4">{item.title}</h1>
                     <p className="text-xl font-medium mb-4">{item.description}</p>
-                    <button className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4">{item.button}</button>
+                    <button className="slider-btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4">{item.button}</button>
                   </div>
                   <section>
                     <img src={item.userProfile} alt={item.user} />

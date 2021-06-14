dd6936c21d9d7c936b039353a9e85ae7d841633b
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-14 23:54:52 +0600

    Update Features.js

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -1,13 +1,28 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Transition from '../utils/Transition.js';
+
 import Slider from 'react-animated-slider';
 import 'react-animated-slider/build/horizontal.css';
 
 function Features() {
 
   const slides = [
-    { title: 'First item', description: 'Lorem ipsum' },
-    { title: 'Second item', description: 'Lorem ipsum' }
+    {
+      title: "Vulputate Mollis Ultricies Fermentum Parturient",
+      description: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+      button: "Read More",
+      image: "https://i.imgur.com/ZXBtVw7.jpg",
+      user: "Luan Gjokaj",
+      userProfile: "https://i.imgur.com/JSW6mEk.png"
+    },
+    {
+      title: "Tortor Dapibus Commodo Aenean Quam",
+      description: "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
+      button: "Discover",
+      image: "https://i.imgur.com/DCdBXcq.jpg",
+      user: "Erich Behrens",
+      userProfile: "https://i.imgur.com/0Clfnu7.png"
+    }
   ];
 
   const [tab, setTab] = useState(1);
@@ -42,9 +57,23 @@ function Features() {
           </div>
 
           <Slider>
-            {slides.map((slide, index) => <div key={index}>
-              <h2>{slide.title}</h2>
-              <div>{slide.description}</div>
+            {slides.map((item, index) => 
+            <div
+              key={index}
+              className="slider-content"
+              style={{ background: `url('${item.image}') no-repeat center center` }}
+            >
+              <div className="inner">
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+                <button>{item.button}</button>
+              </div>
+              <section>
+                <img src={item.userProfile} alt={item.user} />
+                <span>
+                  Posted by <strong>{item.user}</strong>
+                </span>
+              </section>
             </div>)}
           </Slider>
 

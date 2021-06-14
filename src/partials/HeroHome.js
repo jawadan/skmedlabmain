517521e7e494cbe656a86691d3772b19d9bf15f1
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-15 05:07:05 +0600

    btn

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -54,10 +54,10 @@ function HeroHome() {
               /></p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-white bg-yellow-600 hover:bg-ywllow-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Записаться на прием</a>
+                  <a className="btn text-white bg-tellow-600 hover:bg-yellow-700 w-full sm:w-auto sm:ml-4" href="#0">Записаться на прием</a>
                 </div>
                 <div>
-                <a href={pdf} className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" download="Прайслист.pdf">Прайслист</a>
+                <a href={pdf} className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" download="Прайслист.pdf">Скачать Прайслист</a>
                 </div>
               </div>
             </div>

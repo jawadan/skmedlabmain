3d17519aba40f8ec756a133cd020c2d7afa5d034
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-14 08:50:18 +0600

    Update Testimonials.js

--- src/partials/Testimonials.js
+++ src/partials/Testimonials.js
@@ -86,9 +86,9 @@ function Testimonials() {
                 <blockquote className="text-xl font-medium mb-4">
                   “ I love this product and would recommend it to anyone. Could be not easier to use, and our multiple websites are wonderful. We get nice comments all the time. “
                 </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">Darya Finger</cite>
+                <cite className="block font-bold text-lg not-italic mb-1">Турсынова Сауле Кадырхановна</cite>
                 <div className="text-gray-600">
-                  <span>CEO & Co-Founder</span> <a className="text-blue-600 hover:underline" href="#0">@Dropbox</a>
+                  <span>Основатель и Генеральный директор</span> <a className="text-blue-600 hover:underline" href="#0">saule@skmedlab.kz</a>
                 </div>
               </div>
 

ac1a42e5af5580155f038859b5d8d893b5981a47
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-15 08:40:36 +0600

    Update MTable.js

--- src/partials/MTable.js
+++ src/partials/MTable.js
@@ -1,17 +1,21 @@
 import MaterialTable from "material-table";
 import { data } from "./data";
 const MTable = () => {
-  const columns = [
-    { title: "Анализ", field: "title" },
-    { title: "Цена(тг)", field: "price" },
-    { title: "Норма", field: "norm" },
-    { title: "Укороченно", field: "authors" }
+    const columns = [
+        { title: "Анализ", field: "title" },
+        { title: "Цена(тг)", field: "price" },
+        { title: "Норма", field: "norm" },
+        { title: "Укороченно", field: "authors" }
 
-  ];
-  return (
-    <div style={{ maxWidth: "100%" }}>
-      <MaterialTable columns={columns} data={data} title="" />
-    </div>
-  );
+    ];
+    return (
+        <div style={{ maxWidth: "100%" }}>
+            <MaterialTable
+                columns={columns}
+                data={data}
+                title=""
+                options={{ headerStyle: { fontWeight: 'bold', fontSize: '16px' } }} />
+        </div>
+    );
 };
 export default MTable;

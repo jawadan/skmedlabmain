8c7ea622582566f906295a70c54ba16c5a345218
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-16 01:09:37 +0600

    icon

--- src/partials/Newsletter.js
+++ src/partials/Newsletter.js
@@ -321,8 +321,8 @@ function Newsletter() {
                 {/* CTA form */}
                 <form className="w-full lg:w-auto">
                   <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
-                    <input type="email" className="w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-5 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Ваш email" aria-label="Your email…" />
-                    <input type="text" className="w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-5 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Ваш промо ник" aria-label="Your email…" />
+                    <input type="email" className="w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Ваш email" aria-label="Your email…" />
+                    <input type="text" className="w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Ваш промо ник" aria-label="Your email…" />
                     <a className="btn text-white bg-yellow-600 hover:bg-yellow-700 shadow" href="#0">Отправить</a>
                   </div>
                   {/* Success message */}

2aed5969c6c7e57db034addad12fc46df3e0874e
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-16 02:38:21 +0600

    Update MTable.js

--- src/partials/MTable.js
+++ src/partials/MTable.js
@@ -12,7 +12,7 @@ const MTable = () => {
         <div style={{ maxWidth: "100%" }}>
             <MaterialTable
                 components={{
-                    Container: (props) => <Paper {...props} elevation={1} />
+                    Container: (props) => <Paper {...props} elevation={0} />
                 }}
                 columns={columns}
                 data={data}
@@ -21,7 +21,7 @@ const MTable = () => {
                 options={{
                     pageSize: 7,
                     pageSizeOptions: [15, 30, 50],
-                    headerStyle: { fontWeight: 'bold', fontSize: '17px', color: '#191919' }
+                    headerStyle: { fontWeight: 'bold', backgroundColor:"#eaeaea", fontSize: '17px', color: '#191919' }
                 }}
                 localization={{
                     toolbar: {

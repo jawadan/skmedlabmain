7a1ccf9f498b4431a0ba6f3640f8843e075767b3
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-15 10:17:13 +0600

    data

--- src/partials/data.js
+++ src/partials/data.js
@@ -40,5 +40,89 @@ export const data = [
       authors: "Markus Zusak",
       price: 552,
       norm: 4.36
+    },
+    {
+      id: 7,
+      title: "Pride and Prejudice",
+      authors: "Jane Austen",
+      price: 279,
+      norm: 4.25
+    },
+    {
+      id: 8,
+      title: "Twilight",
+      authors: "Stephenie Meyer",
+      price: 498,
+      norm: 3.58
+    },
+    {
+      id: 9,
+      title: "The Book Thief",
+      authors: "Markus Zusak",
+      price: 552,
+      norm: 4.36
+    },
+    {
+      id: 10,
+      title: "Биохимия",
+      authors: "Suzanne Collins",
+      price: 374,
+      norm: 4.33
+    },
+    {
+      id: 11,
+      title: "Глюкоза",
+      authors: "J.K. Rowling",
+      price: 870,
+      norm: 4.48
+    },
+    {
+      id: 12,
+      title: "To Kill a Mockingbird",
+      authors: "Harper Lee",
+      price: 324,
+      norm: 4.27
+    },
+    {
+      id: 13,
+      title: "Pride and Prejudice",
+      authors: "Jane Austen",
+      price: 279,
+      norm: 4.25
+    },
+    {
+      id: 14,
+      title: "Twilight",
+      authors: "Stephenie Meyer",
+      price: 498,
+      norm: 3.58
+    },
+    {
+      id: 15,
+      title: "The Book Thief",
+      authors: "Markus Zusak",
+      price: 552,
+      norm: 4.36
+    },
+    {
+      id: 16,
+      title: "Pride and Prejudice",
+      authors: "Jane Austen",
+      price: 279,
+      norm: 4.25
+    },
+    {
+      id: 17,
+      title: "Twilight",
+      authors: "Stephenie Meyer",
+      price: 498,
+      norm: 3.58
+    },
+    {
+      id: 18,
+      title: "The Book Thief",
+      authors: "Markus Zusak",
+      price: 552,
+      norm: 4.36
     }
   ];

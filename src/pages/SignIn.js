9c7f190869d55b7782a14210c222242abab1e635
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-16 01:38:05 +0600

    Update SignIn.js

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -64,7 +64,7 @@ function SignIn() {
                 <form>
                   <div className="flex flex-wrap -mx-3 mb-3">
                     <div className="w-full px-3">
-                      <button className="btn px-0 text-white bg-gray-900 hover:bg-gray-800 w-full relative flex items-center">
+                      <button className="btn px-0 text-white bg-blue-500 hover:bg-blue-400 w-full relative flex items-center">
                         <svg className="w-8 h-8 fill-current flex-shrink-0 mx-4" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                           <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
                         </svg>
@@ -75,7 +75,7 @@ function SignIn() {
                   <div className="flex flex-wrap -mx-3">
                     <div className="w-full px-3">
                       <button className="btn px-0 text-white bg-red-600 hover:bg-red-700 w-full relative flex items-center">
-                        <svg className="w-4 h-4 fill-current text-white flex-shrink-0 mx-4 ml-3" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                        <svg className="w-8 h-8 fill-current text-white flex-shrink-0 mx-4" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                           <path d="M7.9 7v2.4H12c-.2 1-1.2 3-4 3-2.4 0-4.3-2-4.3-4.4 0-2.4 2-4.4 4.3-4.4 1.4 0 2.3.6 2.8 1.1l1.9-1.8C11.5 1.7 9.9 1 8 1 4.1 1 1 4.1 1 8s3.1 7 7 7c4 0 6.7-2.8 6.7-6.8 0-.5 0-.8-.1-1.2H7.9z" />
                         </svg>
                         <span className="flex-auto pl-16 pr-8 -ml-16">Продолжить с Google</span>

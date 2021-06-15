45d2c76cf52a376f13bc6be848149b72d97f9252
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-16 02:19:02 +0600

    Update MTable.js

--- src/partials/MTable.js
+++ src/partials/MTable.js
@@ -12,7 +12,7 @@ const MTable = () => {
         <div style={{ maxWidth: "100%" }}>
             <MaterialTable
                 components={{
-                    Container: (props) => <Paper {...props} elevation={0.5} />
+                    Container: (props) => <Paper {...props} elevation={1} />
                 }}
                 columns={columns}
                 data={data}

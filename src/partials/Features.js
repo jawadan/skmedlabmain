b513fc7cf7a4ec19bd143d274476d4b406c657ea
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-16 17:54:48 +0600

    Update Features.js

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -68,7 +68,7 @@ function Features() {
 
           {/* Slider */}
           <div className="mb-20">
-            <Slider className="slider-wrapper autoplay={5000}">
+            <Slider className="slider-wrapper" autoplay={5000} touchDisabled>
               {content.map((item, index) => (
                 <div
                   key={index}

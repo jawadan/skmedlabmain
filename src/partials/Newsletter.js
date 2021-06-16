ec638cf3e5a578edf9f53045d9f7a62b306ef3cb
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-16 15:14:14 +0600

    Update Newsletter.js

--- src/partials/Newsletter.js
+++ src/partials/Newsletter.js
@@ -323,7 +323,7 @@ function Newsletter() {
                   <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
                     <input type="email" className="w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Ваш email" aria-label="Your email…" />
                     <input type="text" className="w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Промо ник" aria-label="Your email…" />
-                    <a className="btn text-white bg-yellow-600 hover:bg-yellow-700 shadow" href="#0">Отправить</a>
+                    <input className="btn text-white bg-yellow-600 hover:bg-yellow-700 shadow" type="submit" value="Отправить"/>
                   </div>
                   {/* Success message */}
                   {/* <p className="text-sm text-gray-400 mt-3">Thanks for subscribing!</p> */}

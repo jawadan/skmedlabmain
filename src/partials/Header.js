a6d0d14aec44c568b66aff15fb28fff8083d7e3c
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-18 09:48:05 +0600

    Update Header.js

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -18,8 +18,8 @@ function Header() {
   return (
     <header >
       <TopBar />
-      <div className={`sticky top-0 fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white blur shadow-lg'}`}> 
-      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+      <div className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white blur shadow-lg'}`}> 
+      <div className="sticky top-0 max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
 
           {/* Site branding */}

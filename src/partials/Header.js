8a730fe59ac7bc2cbf14410802273345f259ed81
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-18 08:32:00 +0600

    Update Header.js

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -16,42 +16,23 @@ function Header() {
   }, [top]);
 
   return (
-    <header className={`fixed w-full z-30 md:bg-gray-900 transition duration-300 ease-in-out`}>
-      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white blur shadow-lg'}`}>
+      <div className="max-w-6xl mx-auto px-5 bg-grey-900 sm:px-6">
         <div className="flex items-center justify-between h-8 md:h-10">
 
           {/* Site branding */}
           <div className="flex-shrink-0 mr-4">
             {/* Logo */}
-
           </div>
 
           {/* Site navigation */}
           <nav className="flex flex-grow">
             <ul className="flex flex-grow justify-end flex-wrap items-center">
               <li>
-                <Link to="/blog" target="_blank" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Прайслист</Link>
-              </li>
-              <li>
-                <Link to="/signin" className="btn-sm text-white bg-gray-900 hover:bg-gray-800 ml-3">
-                  <span>Вход</span>
-                  <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link to="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                  <span>Регистрация</span>
-                  <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </svg>
-                </Link>
+                <Link to='/blog' target="_blank" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Прайслист</Link>
               </li>
             </ul>
-
           </nav>
-
         </div>
       </div>
 

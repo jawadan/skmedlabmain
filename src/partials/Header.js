c8315c145934a8094357c2e76ce04349c366dee7
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-18 08:09:55 +0600

    Update Header.js

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-// import pdf from '../assets/docs/pricelist.pdf'
+
 
 function Header() {
 
@@ -18,7 +18,7 @@ function Header() {
   return (
     <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white blur shadow-lg'}`}>
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
-        <div className="flex items-center justify-between h-16 md:h-20">
+        <div className="flex items-center justify-between h-8 md:h-10">
 
           {/* Site branding */}
           <div className="flex-shrink-0 mr-4">
@@ -43,9 +43,9 @@ function Header() {
           {/* Site navigation */}
           <nav className="flex flex-grow">
             <ul className="flex flex-grow justify-end flex-wrap items-center">
-              {/* <li>
-                <Link to={pdf} target="_blank" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out" download="pricelist.pdf">Прайслист</Link>
-              </li> */}
+              <li>
+                <Link to={} target="_blank" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out" download="pricelist.pdf">Прайслист</Link>
+              </li>
               <li>
                 <Link to="/signin" className="btn-sm text-white bg-gray-900 hover:bg-gray-800 ml-3">
                   <span>Вход</span>
@@ -54,20 +54,21 @@ function Header() {
                   </svg>
                 </Link>
               </li>
-              {/* <li>
+              <li>
                 <Link to="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
                   <span>Регистрация</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                   </svg>
                 </Link>
-              </li> */}
+              </li>
             </ul>
 
           </nav>
 
         </div>
       </div>
+
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
 

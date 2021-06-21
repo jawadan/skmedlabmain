1ff641a49d576e076aa3780818712e5e5c452241
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-21 07:19:09 +0600

    Update Testimonials.js

--- src/partials/Testimonials.js
+++ src/partials/Testimonials.js
@@ -120,13 +120,13 @@ function Testimonials() {
           </div>
 
 
-          
-
-          {/* Items */}
-          <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 grid-cols-4 md:grid-cols-5">
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h2 className="h2 mb-4">Наши партнеры</h2>
           </div>
+
+          {/* Items */}
+          <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 grid-cols-4 md:grid-cols-5">
+
             {/* Item */}
             <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
               <svg width="98" height="80" viewBox="0 0 675 549" fill="none" xmlns="http://www.w3.org/2000/svg">

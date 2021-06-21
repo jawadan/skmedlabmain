b8bcaca32688e6653f7772956710aff38246e317
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-21 07:04:24 +0600

    Update Testimonials.js

--- src/partials/Testimonials.js
+++ src/partials/Testimonials.js
@@ -33,7 +33,7 @@ function Testimonials() {
           <div class="w-full max-w-6xl mx-auto">
             <div class="-mx-3 md:flex items-start">
               <div class="px-3 md:w-1/3">
-                {/* <div class="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                <div class="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
                   <div class="w-full flex mb-4 items-center">
                     <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
                       <img src="https://i.pravatar.cc/100?img=1" alt="" />
@@ -45,7 +45,7 @@ function Testimonials() {
                   <div class="w-full">
                     <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sunt ratione dolor exercitationem minima quas itaque saepe quasi architecto vel! Accusantium, vero sint recusandae cum tempora nemo commodi soluta deleniti.<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
                   </div>
-                </div> */}
+                </div>
                 {/* <div class="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
                   <div class="w-full flex mb-4 items-center">
                     <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
@@ -61,7 +61,7 @@ function Testimonials() {
                 </div> */}
               </div>
               <div class="px-3 md:w-1/3">
-                {/* <div class="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                <div class="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
                   <div class="w-full flex mb-4 items-center">
                     <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
                       <img src="https://i.pravatar.cc/100?img=3" alt="" />
@@ -73,8 +73,8 @@ function Testimonials() {
                   <div class="w-full">
                     <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, obcaecati ullam excepturi dicta error deleniti sequi.<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
                   </div>
-                </div> */}
-                <div class="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                </div>
+                {/* <div class="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
                   <div class="w-full flex mb-4 items-center">
                     <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
                       <img src="https://i.pravatar.cc/100?img=4" alt="" />
@@ -86,7 +86,7 @@ function Testimonials() {
                   <div class="w-full">
                     <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto inventore voluptatum nostrum atque, corrupti, vitae esse id accusamus dignissimos neque reprehenderit natus, hic sequi itaque dicta nisi voluptatem! Culpa, iusto.<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
                   </div>
-                </div>
+                </div> */}
               </div>
               <div class="px-3 md:w-1/3">
                 <div class="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
@@ -102,7 +102,7 @@ function Testimonials() {
                     <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, voluptatem porro obcaecati dicta, quibusdam sunt ipsum, laboriosam nostrum facere exercitationem pariatur deserunt tempora molestiae assumenda nesciunt alias eius? Illo, autem!<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
                   </div>
                 </div>
-                <div class="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                {/* <div class="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
                   <div class="w-full flex mb-4 items-center">
                     <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
                       <img src="https://i.pravatar.cc/100?img=6" alt="" />
@@ -114,7 +114,7 @@ function Testimonials() {
                   <div class="w-full">
                     <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto, explicabo, cupiditate quas totam!<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>

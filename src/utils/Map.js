f089b1cf63a0cc4235637418fb404299d74fa177
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-22 01:58:55 +0600

    Update Map.js

--- src/utils/Map.js
+++ src/utils/Map.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-class SimpleMap extends Component {
+class Map extends Component {
   static defaultProps = {
     center: {
       lat: 59.95,
@@ -28,4 +28,4 @@ class SimpleMap extends Component {
   }
 }
 
-export default SimpleMap;
+export default Map;

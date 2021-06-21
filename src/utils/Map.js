e927b60a31909ab52f906b412b0bb03a49708a9c
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-22 02:01:56 +0600

    Update Map.js

--- src/utils/Map.js
+++ src/utils/Map.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Map extends Component {
   static defaultProps = {

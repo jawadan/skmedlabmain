a2dc84bbe60419177d4f35556019fa415d0ceb09
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-27 10:32:23 +0600

    Update Features.js

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -94,6 +94,10 @@ function Features() {
             </Slider>
           </div>
 
+          <div className="absolute inset-0 top-1/2 md:mt-24 lg:mt-0 bg-gray-900 pointer-events-none" aria-hidden="true"></div>
+          <div className="absolute left-0 right-0 bottom-0 m-auto w-px p-px h-20 transform translate-y-1/2"></div>
+
+
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h2 mb-4">Комплексные исследования</h1>
             <p className="text-xl text-gray-600">Своевременно проведенный комплекс исследования - залог успешного лечения.</p>

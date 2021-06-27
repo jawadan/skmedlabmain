0d286fe8a3780a02485fb61f905d9ad249b27ecf
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-27 08:40:34 +0600

    Update Features.js

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -100,7 +100,7 @@ function Features() {
             <div class="">
               <div class="container px-6 py-8 mx-auto">
                 <div class="flex flex-col items-center justify-center space-y-8 lg:-mx-4 lg:flex-row lg:items-stretch lg:space-y-0">
-                  <div class="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
+                  <div class="w-48 relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
                     <div class="flex-shrink-0">
                       <h2
                         class="text-xl font-bold leading-snug tracking-tight mb-3"

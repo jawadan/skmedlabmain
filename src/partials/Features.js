7f8a6d7fd4827332f3616b1de1b1f65818920b5e
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-27 10:56:34 +0600

    Update Features.js

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -94,93 +94,83 @@ function Features() {
             </Slider>
           </div>
 
-          <div class="container flex flex-wrap pt-4 pb-10 m-auto mt-6 md:mt-15 lg:px-12 xl:px-16">
-            <div class="w-full px-0 lg:px-4">
-              <h2 class="px-12 text-base font-bold text-center md:text-2xl text-blue-700">
-                Choose your plan
-              </h2>
-              <p class="py-1 text-sm text-center text-blue-700 mb-10">
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-              </p>
-              <div class="flex flex-wrap items-center justify-center py-4 pt-0">
-                <div class="w-full p-4 md:w-1/2 lg:w-1/4 plan-card">
-                  <label class="flex flex-col rounded-lg shadow-lg group relative cursor-pointer hover:shadow-2xl">
-                    <div class="w-full px-4 py-6 rounded-t-lg card-section-1">
-                      <h3 class="mx-auto text-base font-semibold text-center underline text-blue-500 group-hover:text-white">
-                        Standard
-                      </h3>
-                      <p class="text-5xl font-bold text-center group-hover:text-white text-blue-500">
-                        $25.<span class="text-3xl">95</span>
-                      </p>
-                      <p class="text-xs text-center uppercase group-hover:text-white text-blue-500">
-                        monthly
-                      </p>
-                    </div>
-                    <div
-                      class="flex flex-col items-center justify-center w-full h-full py-6 rounded-b-lg bg-blue-500"
+          <div class="bg-white dark:bg-gray-800">
+            <h1 className="h2 mb-4">Комплексные исследования</h1>
+            <p className="text-xl text-gray-600">Своевременно проведенный комплекс исследования может предостеречь от нежданного.</p>
+            <div class="container px-6 py-8 mx-auto">
+              <div class="flex flex-col items-center justify-center space-y-8 lg:-mx-4 lg:flex-row lg:items-stretch lg:space-y-0">
+                <div class="flex flex-col w-full max-w-sm p-8 space-y-8 text-center bg-white border-2 border-gray-200 rounded-lg lg:mx-4 dark:bg-gray-800 dark:border-gray-700">
+                  <div class="flex-shrink-0">
+                    <h2 class="inline-flex items-center justify-center px-2 font-semibold tracking-tight text-blue-400 uppercase rounded-lg bg-gray-50 dark:bg-gray-700">
+                      Casual
+                    </h2>
+                  </div>
+                  <div class="flex-shrink-0">
+                    <span
+                      class="pt-2 text-4xl font-bold text-gray-800 uppercase dark:text-gray-100"
                     >
-                      <p class="text-xl text-white">
-                        1 month
-                      </p>
-                      <button class="w-5/6 py-2 mt-2 font-semibold text-center uppercase bg-white border border-transparent rounded text-blue-500">
-                        Get Started
-                      </button>
-                    </div>
-                  </label>
+                      Free
+                    </span>
+                  </div>
+
+                  <button
+                    class="inline-flex items-center justify-center px-4 py-2 font-semibold text-white uppercase transition-colors bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none"
+                  >
+                    Start free
+                  </button>
                 </div>
 
-                <div class="w-full p-4 md:w-1/2 lg:w-1/4">
-                  <label class="flex flex-col rounded-lg shadow-lg relative cursor-pointer hover:shadow-2xl">
-                    <div class="w-full px-4 py-8 rounded-t-lg bg-blue-500">
-                      <h3 class="mx-auto text-base font-semibold text-center underline text-white group-hover:text-white">
-                        Premium
-                      </h3>
-                      <p class="text-5xl font-bold text-center text-white">
-                        $21.<span class="text-3xl">95</span>
-                      </p>
-                      <p class="text-xs text-center uppercase text-white">
-                        monthly
-                      </p>
-                    </div>
-                    <div
-                      class="flex flex-col items-center justify-center w-full h-full py-6 rounded-b-lg bg-blue-700"
+                <div class="flex flex-col w-full max-w-sm p-8 space-y-8 text-center bg-white border-2 border-gray-200 rounded-lg lg:mx-4 dark:bg-gray-800 dark:border-gray-700">
+                  <div class="flex-shrink-0">
+                    <h2
+                      class="inline-flex items-center justify-center px-2 font-semibold tracking-tight text-blue-400 uppercase rounded-lg bg-gray-50 dark:bg-gray-700"
                     >
-                      <p class="text-xl text-white">
-                        3 months
-                      </p>
-                      <button class="w-5/6 py-2 mt-2 font-semibold text-center uppercase bg-white border border-transparent rounded text-blue-500">
-                        Save 15%
-                      </button>
-                    </div>
-                  </label>
+                      Profesional
+                    </h2>
+                  </div>
+                  <div class="flex-shrink-0">
+                    <span
+                      class="pt-2 text-4xl font-bold text-gray-800 uppercase dark:text-gray-100"
+                    >
+                      $24.90
+                    </span>
+                    <span class="text-gray-500 dark:text-gray-400">
+                      /month
+                    </span>
+                  </div>
+
+                  <button
+                    class="inline-flex items-center justify-center px-4 py-2 font-semibold text-white uppercase transition-colors bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none"
+                  >
+                    Start free trial
+                  </button>
                 </div>
 
-                <div class="w-full p-4 md:w-1/2 lg:w-1/4 plan-card">
-                  <label class="flex flex-col rounded-lg shadow-lg group card-group relative hover:bg-jblue-secondary cursor-pointer hover:shadow-2xl">
-                    <div class="w-full px-4 py-6 rounded-t-lg card-section-1">
-                      <h3 class="mx-auto text-base font-semibold text-center underline text-blue-500 group-hover:text-white">
-                        Elite
-                      </h3>
-                      <p class="text-5xl font-bold text-center group-hover:text-white text-blue-500">
-                        $19.<span class="text-3xl">45</span>
-                      </p>
-                      <p class="text-xs text-center uppercase group-hover:text-white text-blue-500">
-                        monthly
-                      </p>
-                    </div>
-                    <div
-                      class="flex flex-col items-center justify-center w-full h-full py-6 rounded-b-lg bg-blue-500"
+                <div class="flex flex-col w-full max-w-sm p-8 space-y-8 text-center bg-white border-2 border-gray-200 rounded-lg lg:mx-4 dark:bg-gray-800 dark:border-gray-700">
+                  <div class="flex-shrink-0">
+                    <h2
+                      class="inline-flex items-center justify-center px-2 font-semibold tracking-tight text-blue-400 uppercase rounded-lg bg-gray-50 dark:bg-gray-700"
                     >
-                      <p class="text-xl text-white">
-                        6 months
-                      </p>
-                      <button class="w-5/6 py-2 mt-2 font-semibold text-center uppercase bg-white border border-transparent rounded text-blue-500">
-                        Save 25%
-                      </button>
-                    </div>
-                  </label>
-                </div>
+                      Expert
+                    </h2>
+                  </div>
+                  <div class="flex-shrink-0">
+                    <span
+                      class="pt-2 text-4xl font-bold text-gray-800 uppercase dark:text-gray-100"
+                    >
+                      $49.90
+                    </span>
+                    <span class="text-gray-500 dark:text-gray-400">
+                      /month
+                    </span>
+                  </div>
 
+                  <button
+                    class="inline-flex items-center justify-center px-4 py-2 font-semibold text-white uppercase transition-colors bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none"
+                  >
+                    Start free trial
+                  </button>
+                </div>
               </div>
             </div>
           </div>

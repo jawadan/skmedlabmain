54a935e7a0887f69ae6487f1ae40a6befdc3925b
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-27 10:25:40 +0600

    Update Features.js

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -97,8 +97,8 @@ function Features() {
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h2 mb-4">Комплексные исследования</h1>
             <p className="text-xl text-gray-600">Своевременно проведенный комплекс исследования - залог успешного лечения.</p>
-            <div class="flex">
-              <div class="container px-6 py-8 mx-auto">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6">
+              <div className="py-12 md:py-20">
                 <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
                   <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
                     <h2

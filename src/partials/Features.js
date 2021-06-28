8cb2219d610369828f663928acd0f436eb6707c9
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-29 02:20:38 +0600

    text

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -338,8 +338,8 @@ function Features() {
                   leaveEnd="opacity-0 -translate-y-16"
                 >
                   <div className="relative inline-flex flex-col">
-                    <img className="md:max-w-none mx-auto rounded" src={require('../images/features-bg.png').default} width="530" height="462" alt="Features bg" />
-                    <img className="md:max-w-none absolute w-full left-0" src={require('../images/features-element.png').default} width="530" height="45" alt="Element" style={{ top: '5%' }} /> {/*md:max-w-none absolute w-full left-0 transform animate-float */}
+                    <img className="md:max-w-none mx-auto rounded" src={require('../images/features-element.png').default} width="530" height="462" alt="Features bg" />
+                    {/* <img className="md:max-w-none absolute w-full left-0" src={require('../images/features-element.png').default} width="530" height="45" alt="Element" style={{ top: '5%' }} /> md:max-w-none absolute w-full left-0 transform animate-float */}
                   </div>
                 </Transition>
                 {/* Item 3 */}
